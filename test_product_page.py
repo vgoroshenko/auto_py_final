5017a3b3033c3d98cf7a965df487997298bf8883
test <test@test>
2022-01-31 00:30:34 +0600

    fix

--- test_product_page.py
+++ test_product_page.py
@@ -35,7 +35,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = LoginPage(browser, link)
     page.should_be_login_page()
 
-@pytest.mark.xfail
+#@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
