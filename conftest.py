0c3682c47732f8f223fe3d25d73d05ae6b7718ea
test <test@test>
2022-05-04 14:45:44 +0600

    refactor

--- conftest.py
+++ conftest.py
@@ -1,16 +1,14 @@
+import pytest
 import os
 import allure
-import urllib3
-from selenium.common.exceptions import WebDriverException
 from allure_commons.types import AttachmentType
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.opera.options import Options as OperaOptions
 from selenium.webdriver.safari.options import Options as SafariOptions
-
-import pytest
-from .webdriver_augmented import WebDriverAugmented
 from .settings import config
 
 CHROME_BROWSER_NAME = 'Chrome'
@@ -29,34 +27,36 @@ browser_options = {
     OPERA_BROWSER_NAME: OperaOptions
 }
 
-def desired_caps(browser: str):
-    options = browser_options[browser]()
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--no-sandbox")
-    options.add_argument('--window-size=1920,1080')
-    caps = options
-    return caps
-
-def get_web_driver(browser_name: str) -> WebDriverAugmented:
-    browser = None
-    try:
-        browser = WebDriverAugmented(
-            command_executor=config.webdriver_host,
-            options=desired_caps(browser_name)
-        )
-        browser.browser_name = browser_name
-        browser.page_timer.start()
-    except WebDriverException as e:
-        pytest.exit(print(e))
-    except (urllib3.exceptions.ReadTimeoutError, urllib3.exceptions.NewConnectionError, urllib3.exceptions.MaxRetryError) as e:
-        pytest.exit(print(e))
-    return browser
 
 @pytest.fixture(scope='session', params=test_browsers, ids=lambda x: 'Browser: {}'.format(x))
 def browser(request):
     browser = get_web_driver(request.param)
-    request.addfinalizer(lambda *args: allure.attach(browser.get_screenshot_as_png(), name='name', attachment_type=AttachmentType.PNG) and browser.quit())
+    request.addfinalizer(lambda *args: allure.attach(browser.get_screenshot_as_png(), name='name',
+                                                     attachment_type=AttachmentType.PNG) and browser.quit())
     return browser
+
+
+def desired_caps(browser: str):
+    options = browser_options[browser]()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument('--window-size=1920,1080')
+    caps = options
+    return caps
+
+
+def get_web_driver(browser_name: str):
+    browser = None
+    try:
+        browser = webdriver.Remote(
+            command_executor=config.webdriver_host,
+            options=desired_caps(browser_name)
+        )
+    except WebDriverException as e:
+        pytest.exit(print(e))
+    return browser
+
 
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
 def pytest_runtest_makereport(item, call):
@@ -68,14 +68,14 @@ def pytest_runtest_makereport(item, call):
             with open('failures', mode) as f:
                 if 'browser' in item.fixturenames:
                     web_driver = item.funcargs['browser']
+                    allure.attach(
+                        web_driver.get_screenshot_as_png(),
+                        name='step_screenshot',
+                        attachment_type=allure.attachment_type.PNG
+                    )
                 else:
                     print('Fail to take screen-shot')
                     return
-            allure.attach(
-                web_driver.get_screenshot_as_png(),
-                name='screenshot',
-                attachment_type=allure.attachment_type.PNG
-            )
             if web_driver.browser_name != FIREFOX_BROWSER_NAME:
                 # Firefox do not support js logs: https://github.com/SeleniumHQ/selenium/issues/2972
                 allure.attach(
@@ -83,7 +83,6 @@ def pytest_runtest_makereport(item, call):
                     web_driver.get_screenshot_as_png(),
                     name='console log:',
                     attachment_type=allure.attachment_type.TEXT and allure.attachment_type.PNG
-
                 )
         except Exception as e:
             print('Fail to take screen-shot: {}'.format(e))
--- settings.py
+++ settings.py
@@ -1,6 +1,5 @@
 class Config:
-    # host can be overriden by pytest command line option '--host' (see conftest.py)
-    host = 'localhost'  # Use host.docker.internal to go to local host from selenium grid docker
+    host = 'localhost'
     api_host = 'https://localhost/api'
     webdriver_host = 'http://{}:4444/wd/hub'.format(host)
     local_screenshot_folder = 'screenshots'
